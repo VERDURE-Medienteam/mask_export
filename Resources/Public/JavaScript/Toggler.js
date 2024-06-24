ef9720f9ed426695995810d37efd113660f89bc4
Rafal Jania <rj@studiomitte.com>
2024-06-24 14:16:03 +0200

    [BUGFIX] CSP violation while writing

--- Resources/Public/JavaScript/Toggler.js
+++ Resources/Public/JavaScript/Toggler.js
@@ -10,5 +10,31 @@ define(['jquery'], function($) {
                 $(this).collapse('toggle');
             });
         });
+
+        $('.t3js-modal-trigger').on('click', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+            const TYPO3 = parent.TYPO3;
+
+            TYPO3.Modal.confirm(e.target.dataset.title, e.target.dataset.content, TYPO3.Severity.warning, [
+                {
+                    text: 'Abort!',
+                    active: true,
+                    trigger: function () {
+                        TYPO3.Modal.dismiss();
+                    }
+                }, {
+                    text: 'Overwrite',
+                    active: true,
+                    btnClass: 'btn-primary',
+                    trigger: function (event, modal) {
+                        $('#hidden-submitted').val('install');
+                        $('form').submit();
+                        modal.hideModal();
+                    }
+                }
+            ]);
+        });
     });
 });
